67e2ba7186a91f672f82e44c682166d0a10a74dc
Martin Hoffmann <martin@nlnetlabs.nl>
2022-01-05 11:18:39 +0100

    Include version information.

--- source/conf.py
+++ source/conf.py
@@ -31,9 +31,9 @@ copyright = '2018-2021, NLnet Labs'
 author = 'NLnet Labs (CC-BY 3.0)'
 
 # The short X.Y version
-version = ''
+version = '0.2.0'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '0.2.0-dev'
 
 # -- Version Warning Banner configuration ------------------------------------
 if versionbanner:
