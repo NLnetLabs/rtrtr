a3f1b94d2f79f078e6cc02221c116ff565b97160
Koen <koen@nlnetlabs.nl>
2026-01-06 18:40:55 +0100

    Make new ASPA insertions (announcements & withdrawals) overwrite previous one for that customer AS

--- src/payload.rs
+++ src/payload.rs
@@ -157,6 +157,19 @@ impl PackBuilder {
     /// The method fails with an appropriate error if there already is an
     /// element with the given payload in the set.
     pub fn insert(&mut self, payload: Payload) -> Result<(), PayloadError> {
+        match &payload {
+            // There is only one ASPA, so replace the previous one, whether
+            // it's a withdrawal or announcement.
+            Payload::Aspa(aspa) => {
+                self.items.retain(|item| {
+                    match item {
+                        Payload::Aspa(item) => item.customer != aspa.customer,
+                        _ => true
+                    }
+                });
+            },
+            _ => {}
+        };
         if self.items.insert(payload) {
             Ok(())
         }
